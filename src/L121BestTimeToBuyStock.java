f497ccf0223cf074883830044d7897219677e8bf
Munish Dilip Dhabarde <munishdhabarde@gmail.com>
2026-06-06 23:36:00 +0530

    new problems

--- src/L121BestTimeToBuyStock.java
+++ src/L121BestTimeToBuyStock.java
@@ -1,17 +1,27 @@
+import java.util.*;
 public class L121BestTimeToBuyStock {
+//    public int maxProfit(int[] prices){
+//        int profit = 0;
+//        int low = prices[0];
+//        for(int i = 0; i < prices.length; i++){
+//            if(low > prices[i]){
+//                low = prices[i];
+//            } else if (prices[i]-low > profit) {
+//                profit = prices[i] - low;
+//            }
+//        }
+//
+//
+//
+//        return profit;
+//    }
     public int maxProfit(int[] prices){
         int profit = 0;
-        int low = prices[0];
+        int stockbuy = Integer.MAX_VALUE;
         for(int i = 0; i < prices.length; i++){
-            if(low > prices[i]){
-                low = prices[i];
-            } else if (prices[i]-low > profit) {
-                profit = prices[i] - low;
-            }
+            stockbuy = Math.min(stockbuy, prices[i]);
+            profit = Math.max(profit, prices[i]-stockbuy);
         }
-
-
-
         return profit;
     }
 
